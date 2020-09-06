754a0d5b697605771d30cc34e3eff9dbac878544
yuweiZhan <195812208@qq.com>
2020-09-06 21:32:27 +0800

    :tada: init project!

---
+++ README.md
@@ -0,0 +1,3 @@
+# qiankun插件demo项目
+具体使用访问 https://github.com/F-loat/vue-cli-plugin-qiankun
+
---
+++ qiankun-master/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="container">
+    <header>
+      <nav>
+        <ol>
+          <li v-for="app of apps" :key="app.name">
+            <a @click="goto(app.name, `${app.activeRule}`)">{{app.name}}</a>
+          </li>
+        </ol>
+      </nav>
+    </header>
+    <div id="appContainer" />
+  </div>
+</template>
+
+<script>
+  import { registerMicroApps, runAfterFirstMounted, setDefaultMountApp, start } from 'qiankun'
+
+	export default {
+    name: 'master',
+		data () {
+      return {
+        apps: [
+          { name: 'qiankun-foo-app', entry: '//localhost:8081', container: '#appContainer', activeRule: '/qiankun-foo-app' },
+          { name: 'qiankun-foo-app2', entry: '//localhost:8082', container: '#appContainer', activeRule: '/qiankun-foo-app2' }
+        ]
+      }
+    },
+    created () {
+      if (!window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__) {
+        this.initQiankun()
+      } else {
+        // fix hot-reload
+        location.reload()
+      }
+    },
+		methods: {
+			goto (title, href) {
+				window.history.pushState({}, title, href)
+      },
+      initQiankun () {
+        registerMicroApps(
+          this.apps,
+          {
+            beforeLoad: [
+              app => {
+                // eslint-disable-next-line no-console
+                console.log('before load', app)
+              }
+            ],
+            beforeMount: [
+              app => {
+                // eslint-disable-next-line no-console
+                console.log('before mount', app)
+              }
+            ],
+            afterUnmount: [
+              app => {
+                // eslint-disable-next-line no-console
+                console.log('after unload', app)
+              }
+            ]
+          }
+        )
+
+        setDefaultMountApp('/slave')
+
+        runAfterFirstMounted(() => {
+          // eslint-disable-next-line no-console
+          console.info('first app mounted')
+        })
+
+        start({ prefetch: true })
+      }
+		}
+	}
+</script>
+
+<style scoped>
+  a {
+    color: #42b983;
+    cursor: pointer;
+  }
+  .appContainer {
+    margin-top: 50px;
+  }
+</style>
